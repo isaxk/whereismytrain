25c52bed4ffb01df953de74fa0e5993c74fdf68f
isaxk <76838845+isaxk@users.noreply.github.com>
2025-12-30 20:22:12 +0000

    cache popular destinations

--- src/lib/components/home/popular.svelte
+++ src/lib/components/home/popular.svelte
@@ -4,8 +4,13 @@
 	let list = $state([]);
 
 	async function getPopular(crs: string) {
+		if (localStorage.getItem('popular-' + crs)) {
+			return JSON.parse(localStorage.getItem('popular-' + crs) || '[]');
+		}
 		const response = await fetch(`/api/popular/${crs}`);
 		const data = await response.json();
+		if (!data) return [];
+		localStorage.setItem('popular-' + crs, JSON.stringify(data));
 		return data;
 	}
 
