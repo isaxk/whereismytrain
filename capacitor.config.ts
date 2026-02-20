b5b068b5458889a92a6d656e9a0b4a56761d8b7d
isaxk <76838845+isaxk@users.noreply.github.com>
2026-02-20 15:18:43 +0000

    remove capacitor

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.isaxk.whereismytrain',
-  appName: 'WhereIsMyTrain?',
-  webDir: 'dist'
-};
-
-export default config;
