723038c33fa1e55a4d9c91969e21c0df0b01d89a
isaxk <76838845+isaxk@users.noreply.github.com>
2026-04-30 16:47:32 +0100

    fix share

--- src/routes/(nomap)/share/[focus]/[id]/[[filter]]/+page.svelte
+++ src/routes/(nomap)/share/[focus]/[id]/[[filter]]/+page.svelte
@@ -5,30 +5,14 @@
 
 	let { data } = $props();
 
-	const focus = $derived(data.service.focus);
-	const filter = $derived(data.service.filter);
-
-	const title = $derived.by(() => {
-		if (!filter || !focus) return;
-		if (data.service.destination.some((d) => d.name === filter.name)) {
-			return `${focus.times.plan.dep} ${focus.name} to ${data.service.destination.map((d) => d.name).join(', ')}`;
-		} else {
-			return `${focus.name} to ${filter.name} on the ${focus.times.plan.dep} towards ${data.service.destination.map((d) => d.name).join(', ')}`;
-		}
-	});
-
-	const description = $derived(
-		`Follow this ${data.service.operator.name} service live, on WhereIsMyTrain?`
-	);
-
 	onMount(() => {
 		goto(`/board/${data.focus}/t/${data.id}?to=${data.filter}`);
 	});
 </script>
 
 <svelte:head>
-	<title>{title}</title>
-	<meta property="og:title" content={title} />
-	<meta name="description" content={description} />
-	<meta property="og:description" content={description} />
+	<title>{data.title}</title>
+	<meta property="og:title" content={data.title} />
+	<meta name="description" content={data.description} />
+	<meta property="og:description" content={data.description} />
 </svelte:head>
