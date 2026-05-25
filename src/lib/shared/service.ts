0fc46c571d59e76c3b2d792fee3df5e5bcccc177
isaxk <76838845+isaxk@users.noreply.github.com>
2026-05-25 17:52:58 +0100

    remove dev test code

--- src/lib/shared/service.ts
+++ src/lib/shared/service.ts
@@ -402,8 +402,8 @@ export async function fetchService(
 		isBus: data.serviceType === 'bus' || data.serviceType === 1,
 		operator: {
 			id: data.operatorCode!,
-			// name: operatorList[data.operatorCode!]?.name ?? data.operator ?? 'Unknown',
-			name: data.operator ?? 'Unknown',
+			name: operatorList[data.operatorCode!]?.name ?? data.operator ?? 'Unknown',
+			// name: data.operator ?? 'Unknown',
 			color: operatorList[data.operatorCode!]?.bg ?? '#000000'
 		},
 		title,
